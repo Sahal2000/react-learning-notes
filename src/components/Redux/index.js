c5e871bfd6cd8e45a2eb36681f97153ee5908824
md.sahal <sahal@synesisit.info>
2024-01-03 17:55:54 +0600

    sad

---
+++ src/components/Redux/index.js
@@ -0,0 +1,74 @@
+const {createStore} = require("redux");
+
+
+// defining constants
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const ADD_USER = 'ADD_USER';
+
+// state
+const initialCounterState = {
+    count: 0
+}
+const initialUserState = {
+    users: [
+        {
+            name: "Md Sahal"
+        }
+    ],
+}
+
+// action - object - type,payload
+const incrementCounter = () => {
+    return {
+        type : INCREMENT,
+    };
+};
+
+const decrementCounter = () => {
+    return {
+        type : DECREMENT,
+    };
+};
+
+
+const addUser = () => {
+    return {
+        type : ADD_USER,
+        payload: {name: 'shakil'},
+    };
+};
+
+// create reduce for counter
+const counterReducer = (state = initialCounterState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+
+        default:
+            state;
+    }
+}
+
+// 1. state
+// 2. dispatch action
+// 3. reducer 
+// 4. store - getState(), dispatch(), subscribe()
+
+
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// dispatch action
+store.dispatch(incrementCounter());
